c88c114ec3afb464f038dbd0a1b4b12db5d66908
GaMaF <bieelmagno@gmail.com>
2017-05-27 23:20:52 -0300

    Atualização - Valicação CPF
    Adicionado: Função para validacao de cpf;
    Pode ser enviado para uma biblioteca, que englobe todas as funcoes para diminuir o tamanho do código.

--- Matriz de voos.c
+++ Matriz de voos.c
@@ -1,4 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int validaCpf(char cpf[11], int *valida){
+    int soma=0, resultado, numero, i;
+    char digito10,digito11;
+
+    for(i=0;i<9;i++){
+        numero = cpf[i]-48;
+        soma += (numero*(10-i));
+    }
+
+    resultado=11-(soma%11);
+
+    if(resultado>9)
+        digito10=0;
+    else
+        digito10=resultado;
+
+
+    soma = 0;
+
+    for(i=0;i<9;i++){
+        numero = cpf[i]-48;
+        soma += (numero*(11-i));
+    }
+    soma += digito10*2;
+    resultado = 11-(soma%11);
+
+    if(resultado>9)
+        digito11=0;
+    else
+        digito11=resultado;
+
+    if((digito10 == (cpf[9]-48)) && (digito11 == (cpf[10]-48)))
+        *valida = 1;
+    else
+        *valida = 0;
+}
+
+
 int main()
 {
      struct voo {
